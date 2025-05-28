205b3a95e500b8bb7bb37eb0621715ad233e85e4
k3rielit <70654182+k3rielit@users.noreply.github.com>
2025-05-28 22:34:38 +0200

    Initial prototype

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# nfm-extended-unpacker
-A CLI radq unpacker tool for NFM Extended Edition.
+## Build
+
+1. Have .NET 8.0 LTS SDK installed.
+2. Build the solution.
+3. You should get a RadqExtractor binary.
+
+## Usage
+
+1. Drag and drop one or multiple .radq files onto the executable (or pass absolute paths to .radq files as command line arguments).
+2. All files will be turned back into a regular archive, and put next to the original file with a .zip extension.
---
+++ RadqExtractor/RadqExtractor/Program.cs
@@ -0,0 +1,146 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System.Text;
+using System;
+
+namespace RadqExtractor
+{
+    internal class Program
+    {
+        public static string[] Files { get; set; } = [];
+
+        static async Task Main(string[] args)
+        {
+            var files = args.Where(File.Exists);
+            var tasks = files.Select(ProcessFile);
+            await Task.WhenAll(tasks);
+            Console.WriteLine($"Finished processing {files.Count()} file(s), press any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static async Task ProcessFile(string file)
+        {
+            Console.WriteLine($"Processing file: {file}");
+            try
+            {
+                byte[] inputBytes = await File.ReadAllBytesAsync(file);
+                if (inputBytes.Length == 0)
+                {
+                    Console.WriteLine($"Skipping empty file: {file}");
+                }
+                else
+                {
+                    await DumpCleanedExtendedRadq(inputBytes, file);
+                    Console.WriteLine($"File finished: {file}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"File {file} failed:\n{ex}");
+            }
+        }
+
+        public static byte[] CleanExtendedRadq(byte[] raw)
+        {
+            if (raw[0] == 80 && raw[1] == 75 && raw[2] == 3)
+            {
+                return raw;
+            }
+            for (int i40 = 0; i40 < raw.Length; ++i40)
+            {
+                if (raw[i40] == 75)
+                {
+                    raw[i40] = 85;
+                }
+                else if (raw[i40] == 85)
+                {
+                    raw[i40] = 75;
+                }
+
+                if (raw[i40] == 36)
+                {
+                    raw[i40] = 64;
+                }
+                else if (raw[i40] == 64)
+                {
+                    raw[i40] = 36;
+                }
+
+                if (raw[i40] == 53)
+                {
+                    raw[i40] = 19;
+                }
+                else if (raw[i40] == 19)
+                {
+                    raw[i40] = 53;
+                }
+
+                if (raw[i40] == 21)
+                {
+                    raw[i40] = 44;
+                }
+                else if (raw[i40] == 44)
+                {
+                    raw[i40] = 21;
+                }
+
+                if (raw[i40] == 59)
+                {
+                    raw[i40] = 72;
+                }
+                else if (raw[i40] == 72)
+                {
+                    raw[i40] = 59;
+                }
+
+                if (raw[i40] == 11)
+                {
+                    raw[i40] = 49;
+                }
+                else if (raw[i40] == 49)
+                {
+                    raw[i40] = 11;
+                }
+
+                if (raw[i40] == 13)
+                {
+                    raw[i40] = 68;
+                }
+                else if (raw[i40] == 68)
+                {
+                    raw[i40] = 13;
+                }
+            }
+            return raw;
+        }
+
+        public static async Task DumpCleanedExtendedRadq(byte[] raw, string file)
+        {
+            await File.WriteAllBytesAsync($"{file}.zip", CleanExtendedRadq(raw));
+        }
+
+        public static async Task ExtractExtendedRadq(byte[] raw, string file)
+        {
+            var guid = Guid.NewGuid().ToString();
+            var fileDirectory = $"{Path.GetFileName(file)}_{guid}".Replace(".", "_");
+
+            ZipInputStream zipInputStream = new ZipInputStream(new MemoryStream(CleanExtendedRadq(raw)));
+
+            for (ZipEntry zipentry = zipInputStream.GetNextEntry(); zipentry != null; zipentry = zipInputStream.GetNextEntry())
+            {
+                byte[] result_byte = new byte[zipentry.Size];
+                await zipInputStream.ReadAsync(result_byte);
+
+                string entryName = zipentry.Name;
+                entryName = String.IsNullOrEmpty(entryName) ? guid : entryName;
+
+                string directory = Path.Combine("output", fileDirectory);
+                string outputPath = Path.Combine("output", fileDirectory, entryName);
+                Directory.CreateDirectory(directory);
+                Console.WriteLine($"Entry: {zipentry.Name} Output: {outputPath}");
+
+                await File.WriteAllBytesAsync(outputPath, result_byte);
+            }
+            zipInputStream.Close();
+        }
+    }
+}
